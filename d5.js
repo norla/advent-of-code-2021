6f1f99526c8ddffe3e67e4d823a61234433b111d
Mattias Norlander (EXT Exp) <mattias.norlander@gmail.com>
2021-12-05 15:52:26 +0100

    d5

--- d5.js
+++ d5.js
@@ -6,27 +6,25 @@ function visit(grid, x, y) {
   grid[y][x] = grid[y][x] ? grid[y][x] + 1 : 1;
 }
 
-function run(input, withDiagonals) {
+function run(input) {
   const grid = [];
-  input
-    .filter(([[x1, y1], [x2, y2]]) => x1 === x2 || y1 === y2 || withDiagonals)
-    .forEach(([[x1, y1], [x2, y2]]) => {
-      let [x, y] = [x1, y1];
+  input.forEach(([[x1, y1], [x2, y2]]) => {
+    let [x, y] = [x1, y1];
+    visit(grid, x, y);
+    while (x !== x2 || y !== y2) {
+      x += Math.sign(x2 - x1);
+      y += Math.sign(y2 - y1);
       visit(grid, x, y);
-      while (x !== x2 || y !== y2) {
-        x += Math.sign(x2 - x1);
-        y += Math.sign(y2 - y1);
-        visit(grid, x, y);
-      }
-    });
+    }
+  });
   return grid.flat().filter((n) => n > 1).length;
 }
 
 const lines = fs
   .readFileSync("./d5.txt", "utf-8")
   .trim()
-  .split("\n")
-  .map((str) => str.split(/ -> /).map((coord) => coord.split(",").map(n => Number(n))));
+  .split(/\n/)
+  .map((str) => str.split(/ -> /).map((coord) => coord.split(",").map((n) => Number(n))));
 
-console.log("Part 1", run(lines));
-console.log("Part 2", run(lines, true));
+console.log("Part 1", run(lines.filter(([[x1, y1], [x2, y2]]) => x1 === x2 || y1 === y2)));
+console.log("Part 2", run(lines));
