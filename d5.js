d1f0bce5dbd5c275d1259c091441a50a0c0e6e14
Mattias Norlander (EXT Exp) <mattias.norlander@gmail.com>
2021-12-05 15:56:22 +0100

    d5

--- d5.js
+++ d5.js
@@ -25,6 +25,7 @@ const lines = fs
   .trim()
   .split(/\n/)
   .map((str) => str.split(/ -> /).map((coord) => coord.split(",").map((n) => Number(n))));
+const nonDiagonalLines = lines.filter(([[x1, y1], [x2, y2]]) => x1 === x2 || y1 === y2);
 
-console.log("Part 1", run(lines.filter(([[x1, y1], [x2, y2]]) => x1 === x2 || y1 === y2)));
+console.log("Part 1", run(nonDiagonalLines));
 console.log("Part 2", run(lines));
