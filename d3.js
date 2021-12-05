2a05e84e918db98afb19ecd62905d89cfb5bae44
Mattias Norlander (EXT Exp) <mattias.norlander@gmail.com>
2021-12-05 17:29:29 +0100

    d3 pt 2

--- d3.js
+++ d3.js
@@ -1,21 +1,28 @@
 "use strict";
 const fs = require("fs");
 
-
 const input = fs
   .readFileSync("./d3.txt", "utf-8")
   .trim()
   .split(/\n/)
   .map(line => [...line]);
 
-function getColumn(lines, n) {
-  return lines.map(line => line[n]);
-}
+// Part 1
+const columns = input[0].map((foo, n) => input.map(line => line[n]));
+const {gamma, epsilon} = columns
+      .reduce(({gamma = "", epsilon =""}, col) => {
+        const nOnes = col.filter(x => x === "1").length;
+        return {
+          gamma: gamma + ((nOnes > col.length / 2) ? "1" : "0"),
+          epsilon: epsilon + ((nOnes > col.length / 2) ? "0" : "1")
+        }
+      }, {});
+console.log("Part 1", parseInt(gamma,2) * parseInt(epsilon, 2));
 
+// Part 2
 function getRating(lines, rating, bit = 0) {
-
   if (lines.length === 1) return parseInt(lines[0].join(""), 2);
-  const column = getColumn(lines, bit);
+  const column = lines.map(line => line[bit]);
   const nOnes = column.filter(x => x === "1").length;
   const nZeroes = column.length - nOnes;
   if (rating === "ogr") {
@@ -26,19 +33,4 @@ function getRating(lines, rating, bit = 0) {
     return getRating(lines.filter(line => line[bit] === keep), rating, bit + 1)
   }
 }
-
-
-console.log("RES", getRating(input, "ogr") * getRating(input, "csr"));
-// const gamma = columns
-//       .reduce((acc, col) => {
-//         return  acc + (col.filter(n => n === '0').length < col.length / 2 ? "1" : "0");
-//       }, "");
-// const epsilon = columns
-//       .reduce((acc, col) => {
-//         return  acc + (col.filter(n => n === '0').length > col.length / 2 ? "1" : "0");
-//       }, "");
-
-// console.log("- - - DEBUG input", input);
-
-//console.log("Part 1", parseInt(gamma,2) * parseInt(epsilon, 2));
-// console.log("Part 2", run(lines));
+console.log("Part 2", getRating(input, "ogr") * getRating(input, "csr"));
