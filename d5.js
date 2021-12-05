0a59087daa81b58c8f00e49c39dcd36db556169d
Mattias Norlander (EXT Exp) <mattias.norlander@gmail.com>
2021-12-05 13:29:27 +0100

    d5

--- d5.js
+++ d5.js
@@ -1,50 +1,32 @@
 "use strict";
 const fs = require("fs");
-const _ = require("lodash");
 
-function inclusiveRange(first, last) {
-  return last >= first ? _.range(first, last + 1) : _.range(first, last -1);
+function visit(grid, x, y) {
+  if (!grid[y]) grid[y] = [];
+  grid[y][x] = grid[y][x] ? grid[y][x] + 1 : 1;
 }
 
-function run(withDiagonals) {
-  const input = fs.readFileSync("./d5.txt", "utf-8").trim().split("\n");
-  const lines = input.map(str => str.split(/ -> /).map(coord => coord.split(",").map(_.toNumber)));
+function run(input, withDiagonals) {
   const grid = [];
-  function visit(grid, x,y) {
-    // console.log("- - - DEBUG visit", x, y);
-    if (!grid[y]) grid[y] = []
-    grid[y][x] = grid[y][x] ? grid[y][x] + 1 : 1;
-  }
-  for (const [[x1, y1], [x2, y2]] of lines) {
-   console.log("- - - DEBUG x1,x2,y1,y2", x1,x2,y1,y2, inclusiveRange(x1,x2));
-    if (y1 === y2) {
-      for (const x of inclusiveRange(x1, x2)) visit(grid, x, y1);
-    } else if (x1 === x2) {
-      for (const y of inclusiveRange(y1, y2)) visit(grid, x1, y);
-    } else if (withDiagonals) {
-      const xD = x2 > x1 ? 1 : -1;
-      const yD = y2 > y1 ? 1 : -1;
-      let x = x1, y = y1;
-      console.log("- - - DEBUG x1, x2, xD", x1, x2, xD);
-
-      while(x !== x2) {
+  input
+    .filter(([[x1, y1], [x2, y2]]) => x1 === x2 || y1 === y2 || withDiagonals)
+    .forEach(([[x1, y1], [x2, y2]]) => {
+      let [x, y] = [x1, y1];
+      visit(grid, x, y);
+      while (x !== x2 || y !== y2) {
+        x = x + Math.sign(x2 - x1);
+        y = y + Math.sign(y2 - y1);
         visit(grid, x, y);
-        x = x + xD;
-        y = y + yD;
       }
-    }
-  }
-  print(grid)
-  return grid.flat().filter(n => n > 1).length;
+    });
+  return grid.flat().filter((n) => n > 1).length;
 }
 
-console.log("Part 1", run())
-console.log("Part 2", run(true))
-function print(grid) {
-  for (const line of grid) {
-    for (const n of _.range(0,100)) {
-      process.stdout.write(((line || [])[n]  || ".").toString())
-    }
-    console.log()
-  }
-}
+const lines = fs
+  .readFileSync("./d5.txt", "utf-8")
+  .trim()
+  .split("\n")
+  .map((str) => str.split(/ -> /).map((coord) => coord.split(",").map(n => Number(n))));
+
+console.log("Part 1", run(lines));
+console.log("Part 2", run(lines, true));
