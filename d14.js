efee773d244a84ee3026e8136da1a22cec158fcf
Mattias Norlander (EXT Exp) <mattias.norlander@gmail.com>
2022-12-01 09:49:37 +0100

    d14 correct

--- d14.js
+++ d14.js
@@ -4,43 +4,52 @@ const fs = require("fs");
 const input = fs.readFileSync("./d14.txt", "utf-8").trim().split(/\n/);
 
 const template = input[0];
+const templatePairs = template
+  .split("")
+  .map((c, i, arr) => `${arr[i - 1]}${c}`)
+  .splice(1)
+  .reduce((acc, p) => ({...acc, [p]: (acc[p] || 0) + 1}), {});
 
 const rules = input
   .splice(2)
   .map((l) => l.split(" -> "))
   .reduce((acc, [k, v]) => ({...acc, [k]: v}), {});
 
-function apply(rules, template) {
-  return template.flatMap((c, i) => {
-    if (i === 0) return [c];
-    const key = `${template[i - 1]}${c}`;
-    return [rules[key], c];
-  });
+function apply(rules, pairs, count) {
+  console.log("- - - pair", pairs, count);
+
+  const newPairs = {...pairs};
+  const newCount = {...count};
+  Object.entries(pairs)
+    .filter(([_, n]) => n > 0)
+    .forEach(([pair, n]) => {
+      const newPair1 = `${pair.charAt(0)}${rules[pair]}`;
+      const newPair2 = `${rules[pair]}${pair.charAt(1)}`;
+      // console.log("- - - new pair:", pair, rules[pair], newPair1, newPair2);
+
+      newPairs[pair] = newPairs[pair] - n;
+      newPairs[newPair1] = (newPairs[newPair1] || 0) + n;
+      newPairs[newPair2] = (newPairs[newPair2] || 0) + n;
+      newCount[rules[pair]] = (newCount[rules[pair]] || 0) + n;
+    });
+  return {pairs: newPairs, count: newCount};
+}
+
+function countChars(pairs) {
+  return Object.entries(pairs).reduce((acc, [pair, n], i) => {
+    acc[pair.charAt(1)] = (acc[pair.charAt(1)] || 0) + n;
+    return acc;
+  }, {});
 }
 
 const afterTen = Array(10)
   .fill()
-  .reduce((acc) => apply(rules, acc), template.split(""))
-  .join("");
+  .reduce((acc) => apply(rules, acc.pairs, acc.count), {pairs: templatePairs, count: countChars(templatePairs)});
 
-const freqs = [...afterTen].reduce((acc, c) => ({...acc, [c]: (acc[c] || 0) + 1}), {});
-console.log("Part 1", freqs);
-// Part 1 {
-//   F: 1416,
-//   P: 2957,
-//   H: 1752,
-//   V: 3149,
-//   B: 2400,
-//   S: 1414,
-//   O: 2689,
-//   N: 1966,
-//   K: 789,
-//   C: 925
-// }
+console.log("Part 1", afterTen); // 2957 - 789
 
 const afterForty = Array(40)
   .fill()
-  .reduce((acc, _, i) => console.log(i, acc.length) || apply(rules, acc), template.split(""))
-  .join("");
-const freqs2 = [...afterForty].reduce((acc, c) => ({...acc, [c]: (acc[c] || 0) + 1}), {});
-console.log("Part 2", freqs2);
+  .reduce((acc) => apply(rules, acc.pairs, acc.count), {pairs: templatePairs, count: countChars(templatePairs)});
+const vals = Object.values(afterForty.count).sort((a, b) => a - b);
+console.log("Part 2", vals); // 2967977072188
